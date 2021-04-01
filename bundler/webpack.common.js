2bfff65057e5c323bcad3066a1f4eca28bf3cd3f
OveR1st <315overdose@gmail.com>
2021-04-01 23:42:47 +0300

    Tips for all

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -6,92 +6,115 @@ const path = require('path')
 module.exports = {
     entry: path.resolve(__dirname, '../src/script.js'),
     output:
-    {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, '../dist')
-    },
+        {
+            filename: 'bundle.[contenthash].js',
+            path: path.resolve(__dirname, '../dist')
+        },
     devtool: 'source-map',
     plugins:
-    [
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static') }
-            ]
-        }),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, '../src/index.html'),
-            minify: true
-        }),
-        new MiniCSSExtractPlugin()
-    ],
-    module:
-    {
-        rules:
         [
-            // HTML
-            {
-                test: /\.(html)$/,
-                use: ['html-loader']
-            },
-
-            // JS
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use:
-                [
-                    'babel-loader'
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static') }
                 ]
-            },
-
-            // CSS
-            {
-                test: /\.css$/,
-                use:
+            }),
+            new HtmlWebpackPlugin({
+                template: path.resolve(__dirname, '../src/index.html'),
+                minify: true
+            }),
+            new MiniCSSExtractPlugin()
+        ],
+    module:
+        {
+            rules:
                 [
-                    MiniCSSExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-
-            // Images
-            {
-                test: /\.(jpg|png|gif|svg)$/,
-                use:
-                [
+                    // HTML
                     {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/images/'
-                        }
+                        test: /\.(html)$/,
+                        use: ['html-loader']
+                    },
+
+                    // JS
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use:
+                            [
+                                'babel-loader'
+                            ]
+                    },
+
+                    // CSS
+                    {
+                        test: /\.css$/,
+                        use:
+                            [
+                                MiniCSSExtractPlugin.loader,
+                                'css-loader'
+                            ]
+                    },
+
+                    // Images
+                    {
+                        test: /\.(jpg|png|gif|svg)$/,
+                        use:
+                            [
+                                {
+                                    loader: 'file-loader',
+                                    options:
+                                        {
+                                            outputPath: 'assets/images/'
+                                        }
+                                }
+                            ]
+                    },
+
+                    // Fonts
+                    {
+                        test: /\.(ttf|eot|woff|woff2)$/,
+                        use:
+                            [
+                                {
+                                    loader: 'file-loader',
+                                    options:
+                                        {
+                                            outputPath: 'assets/fonts/'
+                                        }
+                                }
+                            ]
+                    },
+                    // {
+                    //     test: /\.glsl$/,
+                    //     loader: 'webpack-glsl'
+                    // },
+                    {
+                        test: /\.(glsl|frag|vert)$/,
+                        exclude: /node_modules/,
+                        use: [
+                            'raw-loader',
+                            {
+                                loader: 'glslify-loader',
+                                options: {
+                                    transform: [
+                                        ['glslify-hex', { 'option-1': true, 'option-2': 42 }]
+                                    ]
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        test: /\.(glb|gltf)$/,
+                        use:
+                            [
+                                {
+                                    loader: 'file-loader',
+                                    options:
+                                        {
+                                            outputPath: 'assets/models/'
+                                        }
+                                }
+                            ]
                     }
                 ]
-            },
-
-            // Fonts
-            {
-                test: /\.(ttf|eot|woff|woff2)$/,
-                use:
-                [
-                    {
-                        loader: 'file-loader',
-                        options:
-                        {
-                            outputPath: 'assets/fonts/'
-                        }
-                    }
-                ]
-            },
-
-            // Shaders
-            {
-                test: /\.(glsl|vs|fs|vert|frag)$/,
-                exclude: /node_modules/,
-                use: [
-                    'raw-loader'
-                ]
-            }
-        ]
-    }
+        }
 }
--- src/script.js
+++ src/script.js
@@ -1,15 +1,21 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'dat.gui'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
+import {BufferGeometryUtils} from "three/examples/jsm/utils/BufferGeometryUtils";
+
+import Stats from 'stats.js'
+
+
+/**
+ * Stats
+ */
+const stats = new Stats()
+stats.showPanel(0)
+document.body.appendChild(stats.dom)
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -17,92 +23,31 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Loaders
+ * Textures
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
 const textureLoader = new THREE.TextureLoader()
-
-/**
- * Update all materials
- */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            child.material.envMapIntensity = 5
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
-}
-
-/**
- * Environment map
- */
-const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
-])
-environmentMap.encoding = THREE.sRGBEncoding
-
-scene.background = environmentMap
-scene.environment = environmentMap
-
-/**
- * Models
- */
-gltfLoader.load(
-    '/models/DamagedHelmet/glTF/DamagedHelmet.gltf',
-    (gltf) =>
-    {
-        gltf.scene.scale.set(2, 2, 2)
-        gltf.scene.rotation.y = Math.PI * 0.5
-        scene.add(gltf.scene)
-
-        updateAllMaterials()
-    }
-)
-
-/**
- * Lights
- */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 3, - 2.25)
-scene.add(directionalLight)
+const displacementTexture = textureLoader.load('/textures/displacementMap.png')
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -110,7 +55,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
+camera.position.set(2, 2, 6)
 scene.add(camera)
 
 // Controls
@@ -121,35 +66,296 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  powerPreference: 'high-performance',
+  antialias: true
 })
 renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFShadowMap
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 1.5
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setPixelRatio(window.devicePixelRatio)
+
+/**
+ * Test meshes
+ */
+const cube = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(2, 2, 2),
+    new THREE.MeshStandardMaterial()
+)
+cube.castShadow = true
+cube.receiveShadow = true
+cube.position.set(-5, 0, 0)
+// scene.add(cube)
+
+const torusKnot = new THREE.Mesh(
+    new THREE.TorusKnotBufferGeometry(1, 0.4, 128, 32),
+    new THREE.MeshStandardMaterial()
+)
+torusKnot.castShadow = true
+torusKnot.receiveShadow = true
+// scene.add(torusKnot)
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(1, 32, 32),
+    new THREE.MeshStandardMaterial()
+)
+sphere.position.set(5, 0, 0)
+sphere.castShadow = true
+sphere.receiveShadow = true
+// scene.add(sphere)
+
+const floor = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(10, 10),
+    new THREE.MeshStandardMaterial()
+)
+floor.position.set(0, -2, 0)
+floor.rotation.x = -Math.PI * 0.5
+floor.castShadow = true
+floor.receiveShadow = true
+// scene.add(floor)
+
+/**
+ * Lights
+ */
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.normalBias = 0.05
+directionalLight.position.set(0.25, 3, 2.25)
+// scene.add(directionalLight)
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  stats.begin()
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update test mesh
+  // torusKnot.rotation.y = elapsedTime * 0.1
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
+  stats.end()
 }
 
 tick()
+
+/**
+ * Tips
+ */
+
+// // Tip 4
+// console.log(renderer.info)
+
+// // Tip 6
+// scene.remove(cube)
+// cube.geometry.dispose()
+// cube.material.dispose()
+
+// // Tip 10
+// directionalLight.shadow.camera.top = 3
+// directionalLight.shadow.camera.right = 6
+// directionalLight.shadow.camera.left = - 6
+// directionalLight.shadow.camera.bottom = - 3
+// directionalLight.shadow.camera.far = 10
+// directionalLight.shadow.mapSize.set(1024, 1024)
+
+// const cameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(cameraHelper)
+
+// // Tip 11
+// cube.castShadow = true
+// cube.receiveShadow = false
+
+// torusKnot.castShadow = true
+// torusKnot.receiveShadow = false
+
+// sphere.castShadow = true
+// sphere.receiveShadow = false
+
+// floor.castShadow = false
+// floor.receiveShadow = true
+
+// // Tip 12
+// renderer.shadowMap.autoUpdate = false
+// renderer.shadowMap.needsUpdate = true
+
+// // // Tip 18   // создание кубов в одном массиве как 1 (чтобы нарисовать за раз)
+// const geometries = []
+//
+//
+// for (let i = 0; i < 50; i++) {
+//   const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5)
+//
+//   geometry.rotateX((Math.random() - 0.5) * Math.PI * 2)
+//   geometry.rotateY((Math.random() - 0.5) * Math.PI * 2)
+//
+//   geometry.translate(
+//       (Math.random() - 0.5) * 10,
+//       (Math.random() - 0.5) * 10,
+//       (Math.random() - 0.5) * 10,
+//   )
+//   geometries.push(geometry)
+// }
+//
+// const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries(geometries)
+//
+// const material = new THREE.MeshNormalMaterial()
+//
+// const mesh = new THREE.Mesh(mergedGeometry, material)
+//
+// scene.add(mesh)
+// const material = new THREE.MeshNormalMaterial()
+//
+// const mesh = new THREE.Mesh(geometry, material)
+// mesh.position.x = (Math.random() - 0.5) * 10
+// mesh.position.y = (Math.random() - 0.5) * 10
+// mesh.position.z = (Math.random() - 0.5) * 10
+// mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+// mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+//
+// scene.add(mesh)
+// // Tip 19
+// const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5)
+// const material = new THREE.MeshNormalMaterial()
+// for(let i = 0; i < 50; i++)
+// {
+//
+//
+//
+//
+//     const mesh = new THREE.Mesh(geometry, material)
+//     mesh.position.x = (Math.random() - 0.5) * 10
+//     mesh.position.y = (Math.random() - 0.5) * 10
+//     mesh.position.z = (Math.random() - 0.5) * 10
+//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+//
+//     scene.add(mesh)
+// }
+
+// // Tip 20
+// const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5)
+//
+// for(let i = 0; i < 50; i++)
+// {
+//     const material = new THREE.MeshNormalMaterial()
+//
+//     const mesh = new THREE.Mesh(geometry, material)
+//     mesh.position.x = (Math.random() - 0.5) * 10
+//     mesh.position.y = (Math.random() - 0.5) * 10
+//     mesh.position.z = (Math.random() - 0.5) * 10
+//     mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+//     mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+//
+//     scene.add(mesh)
+// }
+
+// // // Tip 22 // для контроля каждого кубика
+// const geometry = new THREE.BoxBufferGeometry(0.5, 0.5, 0.5)
+//
+// const material = new THREE.MeshNormalMaterial()
+//
+// const mesh = new THREE.InstancedMesh(geometry, material, 50)
+// mesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage)
+// scene.add(mesh)
+// for (let i = 0; i < 50; i++) {
+//
+//   const position = new THREE.Vector3(
+//       (Math.random() - 0.5) * 10,
+//       (Math.random() - 0.5) * 10,
+//       (Math.random() - 0.5) * 10,
+//   )
+//
+//   const quaternion = new THREE.Quaternion()
+//   quaternion.setFromEuler(new THREE.Euler(
+//       (Math.random() - 0.5) * Math.PI * 2,
+//       (Math.random() - 0.5) * Math.PI * 2,
+//       0
+//       )
+//   )
+//
+//
+//   const matrix = new THREE.Matrix4()
+//   matrix.makeRotationFromQuaternion(quaternion)
+//   matrix.setPosition(position)
+//   mesh.setMatrixAt(i, matrix)
+//   // const mesh = new THREE.Mesh(geometry, material)
+//   // mesh.position.x = (Math.random() - 0.5) * 10
+//   // mesh.position.y = (Math.random() - 0.5) * 10
+//   // mesh.position.z = (Math.random() - 0.5) * 10
+//   // mesh.rotation.x = (Math.random() - 0.5) * Math.PI * 2
+//   // mesh.rotation.y = (Math.random() - 0.5) * Math.PI * 2
+//   //
+//   // scene.add(mesh)
+// }
+
+// // Tip 29
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Tip 31, 32, 34 and 35
+const shaderGeometry = new THREE.PlaneBufferGeometry(10, 10, 256, 256)
+
+const shaderMaterial = new THREE.ShaderMaterial({
+  precision: 'lowp', // оптимизация
+  uniforms:
+      {
+        uDisplacementTexture: {value: displacementTexture},
+      },
+  defines: {
+    DISPLACMENT_STRENGH: 1.5
+  },
+  vertexShader: `
+        uniform sampler2D uDisplacementTexture;
+        uniform float uDisplacementStrength;
+
+        varying vec2 vUv;
+        varying vec3 vColor;
+
+        void main()
+        {
+             // Position      
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+            float elevation = texture2D(uDisplacementTexture, uv).r;
+            modelPosition.y += max(elevation, 0.5) * DISPLACMENT_STRENGH;
+            gl_Position = projectionMatrix * viewMatrix * modelPosition;
+              
+            // Color
+            float colorElevation = max(elevation, 0.25);
+            vec3 color = mix(vec3(1.0, 0.1, 0.1), vec3(0.1, 0.0, 0.5), colorElevation);
+            
+            // Varying
+            // vUv = uv;
+            vColor = color;
+        }
+    `,
+  fragmentShader: `
+        // varying vec2 vUv;
+        varying vec3 vColor;
+
+        void main()
+        {            
+           // vec3 finalColor = vec3(0.0);
+            // finalColor.r += depthColor.r + (surfaceColor.r - depthColor.r) * elevation;
+            // finalColor.g += depthColor.g + (surfaceColor.g - depthColor.g) * elevation;
+            // finalColor.b += depthColor.b + (surfaceColor.b - depthColor.b) * elevation;
+
+            
+            gl_FragColor = vec4(vColor, 1.0);
+        }
+    `
+})
+
+const shaderMesh = new THREE.Mesh(shaderGeometry, shaderMaterial)
+shaderMesh.rotation.x = -Math.PI * 0.5
+scene.add(shaderMesh)
